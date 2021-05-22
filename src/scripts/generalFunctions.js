3f9378148bf1e19a7303ab63fcee70a0dfe36bd8
VAIO <carranza.tomas64@gmail.com>
2021-05-22 11:33:32 -0300

    Footer

--- src/scripts/generalFunctions.js
+++ src/scripts/generalFunctions.js
@@ -11,3 +11,19 @@ const toggle_menu = document.querySelector('.toggle')
     const deleteLogo = document.querySelector('.logo');
     deleteLogo.classList.toggle("active");
 }
+
+function closeMenu() {
+    const closeToggle = document.querySelector('.toggle');
+    closeToggle.classList.remove("active");
+
+     const closeMenu = document.querySelector('.navegation')
+    closeMenu.classList.remove("active");
+
+
+    const closeLinksRight = document.querySelector('.links-right')
+    closeLinksRight.classList.remove("active");
+
+
+    const close_logo = document.querySelector('.logo');
+   close_logo.classList.remove("active");
+}
